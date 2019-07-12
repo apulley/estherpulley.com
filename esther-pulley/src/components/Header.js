40641e2fd0cbaa29efbd50d1ebfd1026d9937a80
Andrew <andrewpulley@gmail.com>
2019-07-12 14:59:37 -0500

    updates. ep.com

---
+++ esther-pulley/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div>
+            <div className="flex content-center items-center h-16 mx-auto w-4/5">
+                <h1 className="rounded-sm bg-gray-900 text-white font-sans p-2 mr-3">Esther Pulley</h1><div className="text-gray-900 font-sans">Illustration and Design</div>
+            </div>
+        </div>
+    );
+}
+export default Header
