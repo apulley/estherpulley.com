78b98c971aedd759ebc54229852f1ae46f5c5833
Andrew <andrewpulley@gmail.com>
2019-07-12 15:03:40 -0500

    updates

--- esther-pulley/src/components/Header.js
+++ esther-pulley/src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return(
         <div>
             <div className="flex content-center items-center h-16 mx-auto w-4/5">
-                <h1 className="rounded-sm bg-gray-900 text-white font-sans p-2 mr-3">Esther Pulley</h1><div className="text-gray-900 font-sans">Illustration and Design</div>
+                <h1 className="font-bold text-gray-900 font-sans mr-2">Esther Pulley </h1><div className="text-gray-900 font-sans"> Illustration and Design</div>
             </div>
         </div>
     );
